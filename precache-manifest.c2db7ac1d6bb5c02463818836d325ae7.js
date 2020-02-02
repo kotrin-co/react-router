0aa0d70f6fef9d34e0132a61fbb5cf765b40451c
user <user@rrcs-172-254-99-32.nyc.biz.rr.com>
2020-02-02 20:33:53 +0900

    Updates

---
+++ precache-manifest.c2db7ac1d6bb5c02463818836d325ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ab1456b1a961a599ac11ad2beae03c",
+    "url": "/route-practice/index.html"
+  },
+  {
+    "revision": "2ecd4e9a090a0da936f7",
+    "url": "/route-practice/static/js/2.fb4720aa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/route-practice/static/js/2.fb4720aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8975d9464ee271a59df2",
+    "url": "/route-practice/static/js/main.005eeac6.chunk.js"
+  },
+  {
+    "revision": "5cb0229b7793a7e5861f",
+    "url": "/route-practice/static/js/runtime-main.b3accf2b.js"
+  }
+]);
